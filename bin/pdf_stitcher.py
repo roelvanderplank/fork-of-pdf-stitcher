edab0c735f149bfbf4c266ad93fa0ad3de06bba4
roelvanderplank <Roel.van.der.Plank@ordina.nl>
2016-03-02 18:55:55 +0100

    Removed pattern file
    Removed the pattern file, as well as its usage ==> call without the
    pattern.txt file!

--- bin/pdf_stitcher.py
+++ bin/pdf_stitcher.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Copyright addition: code from original project under copyright of that project
-# All changes (lines between "# --- Aanpassing sourcecode" and "# --- Einde aanpassing") released under most permissive licence possible
+# Copyright addition: code from original project(Lageos/pdf-stitcher) under copyright of that project
+# All changes (lines between "# --- Aanpassing sourcecode" and "# --- Einde aanpassing sourcecode") released under most permissive licence possible
 # (whatever that might be)
 from __future__ import print_function, division, with_statement
 import PyPDF2
-import csv
+# --- Aanpassing sourcecode! ---
+# import csv
+# I do not need csv anymore, as I happen to KNOW how many pages I will use...
+# --- Einde aanpassing sourcecode ---
 import sys
 
-__version__ = '0.1'
+# --- Aanpassing sourcecode! ---
+__version__ = '0.2'
+# --- Einde aanpassing sourcecode ---
 # --- Pattern Stitcher ---
 
 def addright(page, right_page, tx=0):
@@ -31,31 +36,40 @@ def main():
         print("Number of Pages: %1.2i" % pdf.getNumPages())
 
         first_page = True
-        with open(sys.argv[2], 'r') as f:
-            reader = csv.reader(f)
-            rows = []
-            for row in reader:
-                for page in row:
-                    print("Reading page %1.2i" % int(page))
-                    if first_page is True:
-                        output = pdf.getPage(int(page) - 1)
-                        first_page = False
-                    else:
-                        output = addright(output, pdf.getPage(int(page) - 1), tx)
-                rows.append(output)
-                first_page = True
-
-            print("Stitching lines of pages ...")
-            first_line = True
-            for row in reversed(rows):
-                if first_line is True:
-                    output = rows[-1]
-                    first_line = False
+        # --- Aanpassing sourcecode! ---
+        # I do not need a csv file, I happen to KNOW which pages to add.
+        # Indeed, it is 'n', 'n+1' all the way to the last page, which is 'n+15'
+        numberPages = pdf.getNumPages()
+        firstPage = numberPages - 15
+        print("First page is: %1.2i" % firstPage)
+        first_page = True
+        rows = []
+        for row in range(0,3):
+            for page in range(0,3):
+                print("Reading page %1.2i" % int(page + row + row + row + row))
+                if first_page is True:
+                    output = pdf.getPage(int(firstPage + row + row + row + row + page - 1))
+                    first_page = False
                 else:
-                    output.mergeTranslatedPage(row, 0, output.mediaBox[3] + ty, expand=True)
+                    output = addright(output, pdf.getPage(int(firstPage + row + row + row + row + page -1)), tx)
+            rows.append(output)
+            first_page = True
+            # --- Einde aanpassing sourcecode ---
+
+        print("Stitching lines of pages ...")
+        first_line = True
+        for row in reversed(rows):
+            if first_line is True:
+                output = rows[-1]
+                first_line = False
+            else:
+                output.mergeTranslatedPage(row, 0, output.mediaBox[3] + ty, expand=True)
 
         print("Saving new file...")
-        with open(sys.argv[3], 'wb') as out_file:
+        # --- Aanpassing sourcecode! ---
+        # As I do not use a 'pattern' file, I only need TWO arguments: inputfile and outputfile. Hence '3'=>'2'
+        with open(sys.argv[2], 'wb') as out_file:
+        # --- Einde aanpassing sourcecode ---
             pdf_out = PyPDF2.PdfFileWriter()
             pdf_out.addPage(output)
             pdf_out.write(out_file)
